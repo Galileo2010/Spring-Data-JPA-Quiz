468a94c66e4c7aa698ca937ecd23a62df76e2922
Galileo2010 <15549070600@163.com>
2019-04-09 19:56:35 +0800

    Complete class Employee and Company.

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,26 @@
 package com.example.employee.entity;
 
 public class Company {
+    private int id;
+    private String companyName;
+    private int employeesNumbers;
 
+    public Company(){}
+    public Company(int id, String companyName, int employeesNumbers){
+        this.id = id;
+        this.companyName = companyName;
+        this.employeesNumbers = employeesNumbers;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public int getEmployeesNumbers() {
+        return employeesNumbers;
+    }
 }
--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,38 @@
 package com.example.employee.entity;
 
 public class Employee {
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private int salary;
 
+    public Employee(){ }
+    public Employee(int id, String name, int age, String gender int companyId, int salary){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
 }
